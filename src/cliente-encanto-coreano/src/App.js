9aed3ede97e39f5ad160e7393f195a523f445a3d
xMilla <xime.a.milla@gmail.com>
2020-04-02 02:14:09 -0300

    api encanto coreano + react

---
+++ src/cliente-encanto-coreano/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//components
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <Button>
+      <h1>Mi react de encanto coreano</h1>
+    </Button>
+    
+  );
+}
+
+export default App;
--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports= {
   "development": {
     "username": "root",
-    "password": "",
+    "password": "mtaileemo29",
     "database": "mi_encanto_coreano",
     "host": "127.0.0.1",
     "dialect": "mysql",
